31e0710bf013992d550251620759a06285472d31
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-22 14:21:02 +0300

    basic validation

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const sha256 = require('crypto-js/sha256');
 
 class Block {
   constructor(index, timestamp, data, previousHash = "") {
@@ -6,16 +6,18 @@ class Block {
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
-    this.hash = this.calculateHash();
+    this.hash = '';
   }
 
   calculateHash() {
-    const hash = crypto.createHash('sha256');
-    hash.update(`${this.index}${this.previousHash}${this.nonce}${this.timestamp}${JSON.stringify(this.data)}`);
-    // hash.update(this.index + this.previousHash + this.nonce + this.timestamp + JSON.stringify(this.data));
-    const base64Hash = hash.digest('base64');
+    // const hash = crypto.createHash('sha256');
+    // hash.update(`${this.index}${this.previousHash}${this.timestamp}${JSON.stringify(this.data)}`);
+    // // hash.update(this.index + this.previousHash + this.nonce + this.timestamp + JSON.stringify(this.data));
+    // const base64Hash = hash.digest('base64');
 
-    return base64Hash;
+    // return base64Hash;
+
+    return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
 
@@ -25,7 +27,10 @@ class Blockchain {
   }
 
   createGenesisBlock() {
-    return new Block(0, new Date(2020, 3, 1), 'Genesis Block', '0');
+    const block = new Block(0, new Date(2020, 3, 1), 'Genesis Block', '0');
+    block.hash = block.calculateHash();
+
+    return block;
   }
 
   getLatestBlock() {
@@ -34,8 +39,26 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
 
     this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+      
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.calculateHash()) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
